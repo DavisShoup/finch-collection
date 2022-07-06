e3f199fd5ba279e6fd5b4c242f1ff19a652ba650
Davis Shoup <davisshoup@yahoo.com>
2022-07-06 19:42:37 -0400

    added.

--- main_app/views.py
+++ main_app/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Finch
 
 # Create your views here.
@@ -12,3 +13,21 @@ def about(request):
 def finches_index(request):
     finches = Finch.objects.all()
     return render(request, 'finches/index.html', {'finches': finches})
+
+def finch_detail(request, finch_id):
+    finch = Finch.objects.get(id=finch_id)
+    return render(request, 'finches/detail.html', { 'finch': finch })
+
+class FinchCreate(CreateView):
+    model = Finch
+    fields = '__all__'
+
+class FinchUpdate(UpdateView):
+    model = Finch
+    fields = ['name', 'breed', 'description']
+
+class FinchDelete(DeleteView):
+    model = Finch
+    success_url = '/finches/'
+
+
