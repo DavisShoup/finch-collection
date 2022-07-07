31cc8e60be267aaa14a3397927c4ab6c927ebd8c
Davis Shoup <davisshoup@yahoo.com>
2022-07-07 17:04:37 -0400

    added

--- main_app/models.py
+++ main_app/models.py
@@ -30,3 +30,9 @@ class Feeding(models.Model):
 
     def __str__(self):
         return f"{self.get_meal_display()} on {self.date}"
+    
+    def __str__(self):
+        return f"{self.get_meal_display()} on {self.date}"
+    
+    class Meta:
+        ordering = ['-date']
